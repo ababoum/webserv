9cadb289e3005d11274c7b85dac990d306f7b326
ababoum <ababou237@hotmail.com>
2022-09-28 18:06:49 +0200

    protection send and recv

--- srcs/ServerEngine.cpp
+++ srcs/ServerEngine.cpp
@@ -6,7 +6,7 @@
 /*   By: mababou <mababou@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/02 18:11:38 by mababou           #+#    #+#             */
-/*   Updated: 2022/09/28 14:54:15 by mababou          ###   ########.fr       */
+/*   Updated: 2022/09/28 17:40:04 by mababou          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -513,7 +513,7 @@ void ServerEngine::stream_in(int poll_client_fd)
 
 	r = recv(client_fd, buffer, REQUEST_BUFFER_SIZE, 0);
 	_globalConf->updateClientFd(client_fd, POLLIN | POLLOUT, this);
-	if (r >= 0)
+	if (r > 0)
 	{
 		_req->getRawData().insert(_req->getRawData().end(), buffer, buffer + r);
 		// we can read more
@@ -587,7 +587,7 @@ int ServerEngine::stream_out(int client_fd)
 
 	// DEBUG("client_fd =\n" << client_fd << '\n' << "to_send =\n" << to_send << '\n');
 
-	if (send(client_fd, to_send.c_str(), to_send.size(), MSG_NOSIGNAL) == -1)
+	if (send(client_fd, to_send.c_str(), to_send.size(), MSG_NOSIGNAL) <= 0)
 		still_alive = 0; // clean
 	if (still_alive)
 		return still_alive;
